bec295859026728900bf5709b81f3d15852a3501
neilpeterson <neilpeterson@outlook.com>
2018-05-14 10:30:59 -0700

    Adding JDK installation for Jenkins

--- jenkins-tutorial/config-jenkins.sh
+++ jenkins-tutorial/config-jenkins.sh
@@ -3,7 +3,10 @@
 # Jenkins
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install oracle-java8-installer -y
 sudo apt-get install jenkins -y
 
 # Docker
