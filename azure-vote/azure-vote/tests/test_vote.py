a95bd33489c19f04da403b2ecf6b6b673e1ccf46
neilpeterson <neilpeterson@outlook.com>
2020-11-06 13:02:45 -0800

    Added moc tests

---
+++ azure-vote/azure-vote/tests/test_vote.py
@@ -0,0 +1,16 @@
+import pytest
+import math
+
+def test_vote1():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def test_vote2():
+   num = 25
+   assert math.sqrt(num) == 5
+
+def test_lint():
+   assert 2*20 == 40
+
+def test_coverage():
+   assert 2*20 == 40
