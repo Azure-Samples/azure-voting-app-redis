b221a5d8f8f210f019ec878396ef13f2dc682263
francesco <francesco.forcher@natzka.com>
2021-11-10 17:04:08 +0100

    90 seconds read timeout in httpx

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -9,7 +9,7 @@ backend_addr = os.environ['COMPUTING_BK']
 async def read_item(msize: int):
     #print(f"Backend address: {backend_addr}")
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"{backend_addr}/loads/matrixinverse?msize={msize}")
+        response = await client.get(f"{backend_addr}/loads/matrixinverse?msize={msize}", timeout=90.0)
         #print(f"Response: {response}")
     return response.json()
 
