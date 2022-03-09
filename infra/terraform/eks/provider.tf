444a954fa5a31eeb6f547fc2f9d153dcc4de019e
Wells Zhang <wells.zhang@binance.com>
2022-03-09 13:01:54 +1300

    add infra module

---
+++ infra/terraform/eks/provider.tf
@@ -0,0 +1,21 @@
+
+   
+terraform {
+  required_version = ">= 0.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72"
+
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 2.2"
+    }
+  }
+}
