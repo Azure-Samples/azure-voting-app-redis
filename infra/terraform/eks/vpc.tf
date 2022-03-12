7c398b316d174250bbd4d316a634118dda12e2d5
Wells Zhang <wells.zhang@binance.com>
2022-03-12 15:47:10 +1300

    Change workspace

--- infra/terraform/eks/vpc.tf
+++ infra/terraform/eks/vpc.tf
@@ -13,7 +13,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-  name                 = "education-vpc"
+  name                 = "edu-#{EnvShort}#-vpc"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
