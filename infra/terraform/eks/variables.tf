a4252e53db48709644a59c6f62b85cfe0c486feb
Wells Zhang <wells.zhang@binance.com>
2022-03-13 15:05:51 +1300

    fixed wrong variable name

--- infra/terraform/eks/variables.tf
+++ infra/terraform/eks/variables.tf
@@ -1,7 +1,14 @@
 variable "env" {
   type = string
+  description = "deployment environment"
 }
 
-variable "aks_name" {
+variable "eks_name" {
   type = string
+  description = "the name of EKS"
+}
+
+variable "region" {
+  type = string
+  description = "AWS Region"
 }
