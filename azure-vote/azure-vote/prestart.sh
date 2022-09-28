c9270c04ddd5f5d8586a5d9e3ccbefa759d713ee
Paul Yu <paul.d.yu@gmail.com>
2022-09-28 11:14:52 -0700

    Update Docker build to support multiple OS architectures

---
+++ azure-vote/azure-vote/prestart.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env sh
+
+echo "Running inside /app/prestart.sh, you could add migrations to this file, e.g.:"
+
+echo "
+#! /usr/bin/env sh
+# Let the DB start
+sleep 10;
+# Run migrations
+alembic upgrade head
+"
